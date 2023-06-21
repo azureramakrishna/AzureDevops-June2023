ce5ba761fe32e5e2572ac7c36e44a0de7e77913c
Ramakrishna <saanvikitmay2023@gmail.com>
2023-06-21 08:03:43 +0200

    Terraform Azure VM

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,6 +1,26 @@
+#azurerm provider version
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.50.0"
+    }
+  }
+}
+
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
+}
+
+# terraform backend state
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "cloud-shell-storage-centralindia"
+    storage_account_name = "csg10032000825eeb72"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
 
 # Create a resource group
@@ -18,6 +38,105 @@ resource "azurerm_storage_account" "example" {
   account_replication_type = "GRS"
 }
 
+
+# Create Virtual Network
+resource "azurerm_virtual_network" "example" {
+  name                = var.virtual_network_name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  address_space       = var.virtual_network_address
+}
+
+# Create Subnet
+resource "azurerm_subnet" "example" {
+  depends_on           = [azurerm_virtual_network.example, azurerm_network_security_group.example]
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = var.subnet_address
+}
+
+# Create public IP
+resource "azurerm_public_ip" "example" {
+  name                = var.publicip_name
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  allocation_method   = "Static"
+}
+
+# Create NSG
+resource "azurerm_network_security_group" "example" {
+  name                = var.network_security_group_name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  security_rule {
+    name                       = "RDP"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# Subnet and NSG Association
+resource "azurerm_subnet_network_security_group_association" "example" {
+  subnet_id                 = azurerm_subnet.example.id
+  network_security_group_id = azurerm_network_security_group.example.id
+}
+
+resource "azurerm_network_interface" "example" {
+  depends_on          = [azurerm_subnet.example, azurerm_public_ip.example]
+  name                = var.network_interface_name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    public_ip_address_id          = azurerm_public_ip.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "example" {
+  depends_on          = [azurerm_network_interface.example]
+  name                = var.virtual_machine_name
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = var.virtual_machine_size
+  admin_username      = var.adminUser
+  admin_password      = var.adminPassword
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  os_disk {
+    name                 = "${var.virtual_machine_name}-osdisk"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2022-Datacenter"
+    version   = "latest"
+  }
+}
+
 output "storage_account_endpoint" {
   value = azurerm_storage_account.example.primary_blob_endpoint
+}
+
+output "winvm_pubip" {
+  value = azurerm_public_ip.example.ip_address
+}
+
+output "winvm_privateip" {
+  value = azurerm_network_interface.example.private_ip_addresses
 }
--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -16,4 +16,59 @@ variable "storage_account_name" {
   default     = "Saanvikit19062023"
 }
 
+variable "virtual_network_name" {
+  type        = string
+  description = "Virtual Network name"
+  default     = "volvo-vnet-01"
+}
 
+variable "virtual_network_address" {
+  type    = list(any)
+  default = ["10.0.0.0/24"]
+}
+
+variable "subnet_name" {
+  type        = string
+  description = "subnet name"
+  default     = "volvo-snet-01"
+}
+
+variable "subnet_address" {
+  type    = list(any)
+  default = ["10.0.0.0/25"]
+}
+
+variable "publicip_name" {
+  type    = string
+  default = "volvo-pubip-01"
+}
+
+variable "network_security_group_name" {
+  type    = string
+  default = "volvo-nsg-01"
+}
+
+variable "network_interface_name" {
+  type    = string
+  default = "volvo-nic-01"
+}
+
+variable "virtual_machine_name" {
+  type    = string
+  default = "volvo-vm-01"
+}
+
+variable "virtual_machine_size" {
+  type    = string
+  default = "Standard_B2s"
+}
+
+variable "adminUser" {
+  type    = string
+  default = "azureuser"
+}
+
+variable "adminPassword" {
+  type    = string
+  default = "Azuredevops@12345"
+}
